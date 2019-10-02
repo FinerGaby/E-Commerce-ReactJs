848aa7617fa240fda79cdd023fc501120d41b4f2
Finer <figueiras120@gmail.com>
2019-10-01 21:14:54 -0300

    Update categorias get

--- frontend/src/component/pages/admin/CategoriaAdd.js
+++ frontend/src/component/pages/admin/CategoriaAdd.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { FetchConsumer } from '../../../context/FetchContext';
+import SidebarAdmin from './SidebarAdmin';
+
+
 
 const CategoriaAdd = () => {
 
@@ -24,17 +28,47 @@ const CategoriaAdd = () => {
        console.log(res)
       }
 
+    let handleDelete = async (id) => {
+        await axios.delete(`http://localhost:8080/categoria/${id}`) 
+        //aqui falta el get para qeu se actualize
+    }
 
+      return (
+      <FetchConsumer>
+            {(value) => { 
+
+            const { categoriasGet } = value;   
+  
 
     return (
+        <React.Fragment>
+            
         <div className="container-tienda">
-            <form onSubmit={handleSubmit}>
-            <label id="idtest" htmlFor="idtest">Agregar categoria:</label>
-            <input type="text" name="id" onChange={handleChange} value={categoria.id}/>
-            <input type="text" name="name" onChange={handleChange} value={categoria.categoria}/>
-            <input type="submit" value="Crear" />
-            </form>
+        <div className="container-tienda-flex">
+                    <SidebarAdmin />
+            <div>
+                <form onSubmit={handleSubmit}>
+                        <label id="idtest" htmlFor="idtest">Agregar categoria:</label>
+                        <input type="text" name="id" onChange={handleChange} value={categoria.id}/>
+                        <input type="text" name="name" onChange={handleChange} value={categoria.categoria}/>
+                        <input type="submit" value="Crear" />
+                 </form>
+
+                {
+                categoriasGet.map((cat, index) => 
+                    <div>
+                        {cat.name}
+                        <div class="boton-delete" onClick={() => handleDelete(cat.id)}>delete cat</div>
+                    </div>
+                        )
+                }
+            </div>
         </div>
+        </div>
+
+        </React.Fragment>
+        )}}
+        </FetchConsumer>
     )
 }
 
